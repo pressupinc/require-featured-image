c510ca5c32efe91bc8c17ddbb00853577afaad0e
David Hayes <dhayes@gmail.com>
2013-08-01 12:11:20 -0600

    First commit

---
+++ check.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+
+    if ($('body').find("#nofeature-message").length===0) {
+		$('h2').after('<div id="nofeature-message"></div>');
+    }
+
+	if ($('body').find("#categorydiv").length===1) {
+		setInterval(detectWarnFeaturedImage, 5000);
+		detectWarnFeaturedImage();
+	}
+
+    function detectWarnFeaturedImage() {
+		if( $('#postimagediv').find('img').length===0 ) {
+			$('#nofeature-message').addClass("error").html('<p><strong>This post has no featured image.</strong> Please set one. You must set a featured image before publishing your post.</p>');
+			$('#publish').attr('disabled','disabled');
+		} else {
+			$('#nofeature-message').remove();
+			$('#publish').removeAttr('disabled');
+		}
+	}
+});

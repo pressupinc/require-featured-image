0bfd95ddae09da5b94b0d9695802f27119076e2d
David Hayes <dhayes@gmail.com>
2015-01-24 16:52:17 -0700

    Big refactoring of the Javascript to make it obvious what it going on, and to make it only disable buttons and warn on posts that aren't already published

--- require-featured-image-on-edit.js
+++ require-featured-image-on-edit.js
@@ -1,21 +1,45 @@
 jQuery(document).ready(function($) {
 
-    if ($('body').find("#nofeature-message").length===0) {
-		$('h2').after('<div id="nofeature-message"></div>');
-    }
+	function postTypeSupportsFeaturedImage() {
+		return $.find('#postimagediv').length !== 0;
+	}
 
-	setInterval(detectWarnFeaturedImage, 5000);
-	detectWarnFeaturedImage();
+	function hasFeaturedImage() {
+		return $('#postimagediv').find('img').length === 0;
+	}
+
+	function publishButtonIsPublishText() {
+		return $('#publish').attr('value') === 'Publish';
+	}
+
+	function disablePublishAndWarn() {
+		createMessageAreaIfNeeded();
+		$('#nofeature-message').addClass("error")
+			.html('<p>'+objectL10n.jsWarningHtml+'</p>');
+		$('#publish').attr('disabled','disabled');
+	}
+
+	function clearWarningAndEnablePublish() {
+		$('#nofeature-message').remove();
+		$('#publish').removeAttr('disabled');
+	}
+
+	function createMessageAreaIfNeeded() {
+		if ($('body').find("#nofeature-message").length === 0) {
+			$('h2').after('<div id="nofeature-message"></div>');
+	    }
+	}
 
     function detectWarnFeaturedImage() {
-		if ($.find('#postimagediv').length !== 0) {
-			if ($('#postimagediv').find('img').length===0 ) {
-				$('#nofeature-message').addClass("error").html('<p>'+objectL10n.jsWarningHtml+'</p>');
-				$('#publish').attr('disabled','disabled');
+		if (postTypeSupportsFeaturedImage()) {
+			if (hasFeaturedImage() && publishButtonIsPublishText()) {
+				disablePublishAndWarn();
 			} else {
-				$('#nofeature-message').remove();
-				$('#publish').removeAttr('disabled');
+				clearWarningAndEnablePublish();
 			}
 		}
 	}
+
+	detectWarnFeaturedImage();
+	setInterval(detectWarnFeaturedImage, 3000);
 });

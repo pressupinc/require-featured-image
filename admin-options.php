47f3d9ba8467026b2fb0944a418ba976c255afb5
David Hayes <dhayes@gmail.com>
2016-02-08 16:12:38 -0700

    Code style cleanup

--- admin-options.php
+++ admin-options.php
@@ -60,13 +60,13 @@ function rfi_return_post_types_which_support_featured_images() {
 	return $return;
 }
 
-function rfi_return_min_dimensions(){
+function rfi_return_min_dimensions() {
 	$minimum_size = get_option('rfi_minimum_size');
-	if (isset($minimum_size["width"]) && $minimum_size["width"] == 0) {
-		$minimum_size["width"] = 0;
+	if (isset($minimum_size['width']) && $minimum_size['width'] == 0) {
+		$minimum_size['width'] = 0;
 	}
-	if (isset($minimum_size["height"]) && $minimum_size["height"] == 0) {
-		$minimum_size["height"] = 0;
+	if (isset($minimum_size['height']) && $minimum_size['height'] == 0) {
+		$minimum_size['height'] = 0;
 	}
 	return $minimum_size;
 }
@@ -86,6 +86,6 @@ function rfi_post_types_input_renderer() {
 
 function rfi_size_option_renderer(){
 	$dimensions = rfi_return_min_dimensions();
-	echo '<input type="number" name = "rfi_minimum_size[width]", value = "'.$dimensions["width"].'"> width (px) <br>';
-	echo '<input type="number" name = "rfi_minimum_size[height]", value = "'.$dimensions["height"].'"> height (px)<br>';
+	echo '<input type="number" name="rfi_minimum_size[width]", value="'.$dimensions["width"].'"> width (px) <br>';
+	echo '<input type="number" name="rfi_minimum_size[height]", value="'.$dimensions["height"].'"> height (px)<br>';
 }
